3d22daf739e7a4bfb744e4f22ce4cf6f90a3a2f2
UmbertoDV <biccoumby2002@hotmail.com>
2023-01-31 09:44:21 +0100

    +1 on random

--- JSnack1/js/main.js
+++ JSnack1/js/main.js
@@ -2,6 +2,7 @@ const numeroMinimoChiesto = parseInt(prompt("Inserisci il numero Minimo"));
 const numeroMassimoChiesto = parseInt(prompt("Inserisci il numero Massimo"));
 
 const numeroDavveroMassimo = numeroMassimoChiesto - numeroMinimoChiesto;
-random = Math.floor(Math.random() * numeroDavveroMassimo) + numeroMinimoChiesto;
+random =
+	Math.floor(Math.random() * numeroDavveroMassimo + 1) + numeroMinimoChiesto;
 
 console.log(random);
