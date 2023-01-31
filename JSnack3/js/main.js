85981f97fd7ce299462709ffe4323f3635565c9b
UmbertoDV <biccoumby2002@hotmail.com>
2023-01-31 10:37:00 +0100

    adjust

--- JSnack3/js/main.js
+++ JSnack3/js/main.js
@@ -1,13 +1,12 @@
 const numeroDiArray = parseInt(prompt("inserisci i numeri di array"));
-let array = [];
 let arrayGenerati = 0;
 
 while (arrayGenerati < numeroDiArray) {
+	let array = [];
 	for (let i = 0; i < 10; i++) {
 		const randomNumber = Math.floor(Math.random() * 100) + 1;
 		array.push(randomNumber);
 	}
 	arrayGenerati++;
 	console.log(array);
-	array = [];
 }
